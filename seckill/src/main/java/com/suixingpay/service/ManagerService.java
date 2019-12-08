6c3e07c30a2bd146358d3c71650b42f863097ddf
李常昊 <lichanghaoshehao@163.com>
2019-12-08 17:55:02 +0800

    shabi

--- seckill/src/main/java/com/suixingpay/service/ManagerService.java
+++ seckill/src/main/java/com/suixingpay/service/ManagerService.java
@@ -3,6 +3,13 @@ package com.suixingpay.service;
 
 public interface ManagerService {
 int i = 1000000000;
+    int j = 1000000000;
+    int k = 1000000000;
+    int l = 1000000000;
+    int a= 1000000000;
+    int s = 1000000000;
+
+
 
 
 }
