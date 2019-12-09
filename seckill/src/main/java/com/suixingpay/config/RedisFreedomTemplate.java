90b54b53ec863a2f20d01356ff4bcda2ebcf4980
wz1996-png <wen_zhuang@suixingpay.com>
2019-12-09 20:28:06 +0800

    文状修改

--- seckill/src/main/java/com/suixingpay/config/RedisFreedomTemplate.java
+++ seckill/src/main/java/com/suixingpay/config/RedisFreedomTemplate.java
@@ -1,41 +1,41 @@
-//package com.suixingpay.config;
-//
-//import com.fasterxml.jackson.annotation.JsonAutoDetect;
-//import com.fasterxml.jackson.annotation.PropertyAccessor;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-//
-///**
-// * @program: seckillusers
-// * @description: 自定义redis的模板
-// * @author: 文状
-// * @create: 2019-12-09 14:38
-// **/
-//public class RedisFreedomTemplate {
-//    //RedisTemplate默认采用的是JDK的序列化策略，保存的key和value都是采用此策略序列化保存的。
-//    //springboot封装了RedisTemplate对象来对redis进行各种操作
-//    @Bean
-//    @ConditionalOnMissingBean(RedisTemplate.class)
-//    public RedisTemplate<String, Object> initRedisFreeTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<String, Object> redis = new RedisTemplate<>();
-//        redis.setConnectionFactory(connectionFactory);
-//
-//        //定义key-value的序列化方式:jackson2
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-//        ObjectMapper om = new ObjectMapper();
-//        //该函数用于改变视图的
-//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        jackson2JsonRedisSerializer.setObjectMapper(om);
-//
-//        redis.setKeySerializer(jackson2JsonRedisSerializer);
-//        redis.setValueSerializer(jackson2JsonRedisSerializer);
-//        redis.afterPropertiesSet();
-//
-//        return redis;
-//    }
-//}
+package com.suixingpay.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+/**
+ * @program: seckillusers
+ * @description: 自定义redis的模板
+ * @author: 文状
+ * @create: 2019-12-09 14:38
+ **/
+public class RedisFreedomTemplate {
+    //RedisTemplate默认采用的是JDK的序列化策略，保存的key和value都是采用此策略序列化保存的。
+    //springboot封装了RedisTemplate对象来对redis进行各种操作
+    @Bean
+    @ConditionalOnMissingBean(RedisTemplate.class)
+    public RedisTemplate<String, Object> initRedisFreeTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redis = new RedisTemplate<>();
+        redis.setConnectionFactory(connectionFactory);
+
+        //定义key-value的序列化方式:jackson2
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        //该函数用于改变视图的
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        redis.setKeySerializer(jackson2JsonRedisSerializer);
+        redis.setValueSerializer(jackson2JsonRedisSerializer);
+        redis.afterPropertiesSet();
+
+        return redis;
+    }
+}
