9158e0a349a1eae43216c72b644fd0679499d26e
sunkeqiang <1830716357@qq.com>
2019-12-09 13:51:22 +0800

    登录拦截暂时注掉--孙克强

--- seckill/src/main/java/com/suixingpay/config/MyMvcConfig.java
+++ seckill/src/main/java/com/suixingpay/config/MyMvcConfig.java
@@ -8,27 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /*
  *@Author 孙克强
  */
-@Configuration
-class MyMvcConfig implements WebMvcConfigurer {
-
-    //配置映射路径
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    //配置拦截器
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new MyHandlerIntercepter())
-                .excludePathPatterns("/login/**")
-                .excludePathPatterns("/swagger-ui.html")
-                .excludePathPatterns("swagger-ui.html")
-                .excludePathPatterns("/webjars/**")
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg", "/*.html", "/**/*.html","/swagger-resources/**")
-                .addPathPatterns("/**");
-    }
-
-}
+//@Configuration
+//class MyMvcConfig implements WebMvcConfigurer {
+//
+//    //配置映射路径
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//
+//    //配置拦截器
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new MyHandlerIntercepter())
+//                .excludePathPatterns("/login/**")
+//                .excludePathPatterns("/swagger-ui.html")
+//                .excludePathPatterns("swagger-ui.html")
+//                .excludePathPatterns("/webjars/**")
+//                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg", "/*.html", "/**/*.html","/swagger-resources/**")
+//                .addPathPatterns("/**");
+//    }
+//}
