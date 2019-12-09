9b3864e922d0f981b7c5ddd909ff9f18023dfbc0
Zhangjiaxin <1454006007@qq.com>
2019-12-09 16:41:43 +0800

    提交Impl

--- seckill/src/main/java/com/suixingpay/service/serviceimpl/ManagerMessageServiceImpl.java
+++ seckill/src/main/java/com/suixingpay/service/serviceimpl/ManagerMessageServiceImpl.java
@@ -19,13 +19,19 @@ public class ManagerMessageServiceImpl implements ManagerMessageService {
     @Autowired
     private SilentuserRepository silentuserRepository;
 
-    //查询鑫管家抢到的用户通知
+    /**
+     * 张佳鑫
+     * 查询鑫管家抢到的所有用户通知
+     */
     @Override
     public List<Silentuser> searchAllUserInfo(Integer managerId) {
         return silentuserRepository.selectByManagerId(String.valueOf(managerId));
     }
 
-    //查询抢到的沉默用户
+    /**
+     * 张佳鑫
+     * 查询鑫管家抢到的沉默用户详细
+     */
     @Override
     public Silentuser searchUserInfo(Integer userId) {
         return silentuserRepository.selectById(String.valueOf(userId));
