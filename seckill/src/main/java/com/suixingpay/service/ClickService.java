f442820e0b709831ed8496b881294f4a7bac55b7
Mrs.yao <s17745134408@163.com>
2019-12-09 17:39:43 +0800

    ClickService

---
+++ seckill/src/main/java/com/suixingpay/service/ClickService.java
@@ -0,0 +1,18 @@
+package com.suixingpay.service;
+
+import com.suixingpay.entity.Manager;
+import com.suixingpay.utils.GenericResponse;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @ClassName ClickService
+ * @Description TODO
+ * @Author ShiMengyao
+ * @Date 2019 年 12 月 8 日 0008 17:24:54
+ * @Version 1.0
+ */
+public interface ClickService {
+  Manager Click(Integer managerId);
+  //
+}
