e968462a58b70f5b923c2759d2b012aa5b8342d6
Zhangjiaxin <1454006007@qq.com>
2019-12-09 16:36:05 +0800

    提交controller

--- seckill/src/main/java/com/suixingpay/controller/ManagerMessageController.java
+++ seckill/src/main/java/com/suixingpay/controller/ManagerMessageController.java
@@ -4,6 +4,8 @@ import com.suixingpay.entity.Manager;
 import com.suixingpay.entity.Silentuser;
 import com.suixingpay.service.ManagerMessageService;
 import com.suixingpay.utils.GenericResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +18,15 @@ import java.util.concurrent.Callable;
  */
 @RestController
 @RequestMapping("/managermessage")
+@Api("鑫管家查询已抢到用户模块")
 public class ManagerMessageController {
 
     @Autowired
     private ManagerMessageService managerMessageService;
 
-    //查询鑫管家抢到的用户通知
+    //根据鑫管家id查询鑫管家抢到的所有用户通知
     @GetMapping("/searchAllUserInfo/{managerId}")
+    @ApiOperation(value = "鑫管家查询已抢到用户模块",notes = "根据鑫管家id查询鑫管家抢到的所有用户通知")
     public Callable<GenericResponse> searchAllUserInfo(@PathVariable("managerId") Integer managerId){
         //Manager manager = (Manager) request.getSession().getAttribute(String.valueOf(request.getHeader("manage_Id")));
         //Integer managerId = manager.getManageId();
@@ -39,6 +43,7 @@ public class ManagerMessageController {
     public Callable<GenericResponse> searchUserInfo(@PathVariable("userId") Integer userId){
         Silentuser silentuser = managerMessageService.searchUserInfo(userId);
         if (silentuser != null) {
+
             return () -> GenericResponse.success("searchUserInfo666", "查询成功", silentuser);
         } else {
             return () -> GenericResponse.failed("searchUserInfo999", "查询失败");
