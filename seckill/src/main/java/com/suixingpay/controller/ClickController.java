aa60d0995b34747c2c6823cdbe1ccab7a144d71e
Mrs.yao <s17745134408@163.com>
2019-12-11 16:44:51 +0800

    判断鑫管家点击进入活动页面加入城市判断--石梦瑶

--- seckill/src/main/java/com/suixingpay/controller/ClickController.java
+++ seckill/src/main/java/com/suixingpay/controller/ClickController.java
@@ -33,7 +33,7 @@ public class ClickController {
     private static int count = -1;
 
     @GetMapping("/clickRob/{sceneId}/{managerId}")
-    public Callable<GenericResponse> clickRob(@PathVariable("sceneId") Integer sceneId, @PathVariable Integer managerId) throws ParseException {
+    public Callable<GenericResponse> clickRob(@PathVariable("sceneId") Integer sceneId, @PathVariable("managerId") Integer managerId) throws ParseException {
 
         String noting = "今日用户已经被抢完，请留意后续活动";
         String joined = "已经参加活动，请等待结果公布";
@@ -43,23 +43,26 @@ public class ClickController {
         String endPoint = "目前无活动，敬请期待";
         String end = "今天全部活动已经结束";
         String start = "活动还没有开始";
-        if (count == -1) {
-            synchronized (this) {
-                if (count == -1) {
-                    count = sceneService.selectById(sceneId).getSceneCount();
-                    System.out.println("总数为" + count);
-                }
+        String without = "没有沉默用户可以被抢购";
+        String different = "您的归属地不在本次活动范围内";
+
+        synchronized (this) {
+            if (count == -1) {
+                count = sceneService.selectById(sceneId).getSceneCount();
+                System.out.println("总数为" + count);
             }
+
             //判断当前时间
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             //获取活动的结束时间
             String s = sceneService.selectById(sceneId).getSceneEndtime();
-            System.out.println(s);
             //获取活动的开始时间
             String starttime = sceneService.selectById(sceneId).getSceneStarttime();
             Date date = null;
             date = sdf.parse(s);//数据库中的活动结束时间
 
+        if (managerService.selectById(managerId).getManageProvince().equals(sceneService.selectById(sceneId).getSceneProvince())) {
+                //如果鑫管家和活动城市相同
             //判断活动是否结束  当前时间是否在活动结束时间之后
             if (new Date().after(date)) {
                 //如果这次活动下面没有活动了 返回目前没有活动
@@ -70,8 +73,7 @@ public class ClickController {
                 String sceneEndtime = sc.getSceneEndtime().substring(0, 10);
                 Scene sc1 = sceneService.selectById((sceneId + 1));
                 String sceneEndtime1 = sc1.getSceneEndtime().substring(0, 10);
-
-
+                //当前结束时间和下一场的活动时间是否在同一天。
                 if (sceneEndtime.equals(sceneEndtime1)) {
                     //下一场的的城市跟鑫管家的所在城市是否在一个地方
                     if (managerService.selectById(managerId).getManageProvince().equals(sceneService.selectById((sceneId + 1)).getSceneProvince())) {
@@ -88,21 +90,24 @@ public class ClickController {
                     return () -> GenericResponse.success("selectById666", "活动结束", endPoint);
                 }
 
-            } else if ((managerService.selectById(managerId).getManageIsgrab() == 0) && count > 0 && new Date().before(date) && new Date().after(sdf.parse(starttime))) {
-                //判断鑫管家有没有领到用户，等于0 表示可以抢用户
-                if (sceneService.selectById(sceneId).getSceneCount() > 0) {
-                    //沉默用户大于0 写抢的代码
-                    synchronized (this) {
-                        count--;
-                        if (count < 0) {
-                            return () -> GenericResponse.success("click666", "失败", noting);
+            } else if (managerService.selectById(managerId).getManageIsgrab() == 0 && count > 0 && new Date().before(date) && new Date().after(sdf.parse(starttime))) {
+
+                    if (sceneService.selectById(sceneId).getSceneCount() > 0) {
+                        //沉默用户大于0 写抢的代码
+                        synchronized (this) {
+                            count--;
+                            if (count < 0) {
+                                return () -> GenericResponse.success("click666", "失败", noting);
+                            }
+                            //更改管家领取用户的状态
+                            managerService.updateManageByManageId(managerId);
+                            System.out.println("已经抢走一个用户，还剩" + count);
+                            return () -> GenericResponse.success("click666", "成功", success);
                         }
-                        //更改管家领取用户的状态
-                        managerService.updateManageByManageId(managerId);
-                        System.out.println("已经抢走一个用户，还剩" + count);
-                        return () -> GenericResponse.success("click666", "成功", success);
+                    } else {
+                        //最开始就没有沉默用户
+                        return () -> GenericResponse.success("click666", "失败", without);
                     }
-                }
             } else if (managerService.selectById(managerId).getManageIsgrab() == 1) {
                 //表示==1 鑫管家已经领过一次了，
                 System.out.println("已经参加活动");
@@ -110,8 +115,10 @@ public class ClickController {
             } else {
                 return () -> GenericResponse.success("click", "活动还没有开始", start);
             }
-
+        } else {
+            return () -> GenericResponse.success("click", "归属地不在活动范围内", different);
         }
-        return null;
+        }
     }
 }
+
