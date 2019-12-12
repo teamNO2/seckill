cfc255ece064318387b5af5387a5c1957026ab81
Zhangjiaxin <1454006007@qq.com>
2019-12-12 17:28:28 +0800

    张佳鑫提交ser

--- seckill/src/main/java/com/suixingpay/service/SceneService.java
+++ seckill/src/main/java/com/suixingpay/service/SceneService.java
@@ -2,6 +2,7 @@ package com.suixingpay.service;
 
 import com.suixingpay.entity.Scene;
 import com.suixingpay.entity.Silentuser;
+import io.swagger.models.auth.In;
 
 import java.util.List;
 
@@ -26,5 +27,11 @@ public interface SceneService {
      * 张佳鑫
      * 统计用户资源
      */
-    List<Silentuser> selectUserResource();
+    List<Silentuser> selectUserResource(String curTime);
+
+    /*
+     * 张佳鑫
+     * 查询每场活动的结束时间
+     */
+    Integer selectEndTime(String sceneEndTime);
 }
